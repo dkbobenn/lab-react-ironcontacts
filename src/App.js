b5d01d009aba6bc8760dbd58507a08b5c084fe44
Bo Bennetsen <Bo.Bennetsen@LEGO.com>
2022-02-22 19:26:29 +0100

    First commit - Iteration 1 and 2 completed

--- src/App.js
+++ src/App.js
@@ -1,23 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Allcontacts from "./contacts.json";
+import { useState } from "react";
+
+const contacts = Allcontacts.slice(0, 20);
+//console.log(contacts);
+const randomContact =
+  Allcontacts[Math.floor(Math.random() * Allcontacts.length)];
+console.log(`This is randomContact = ${randomContact.name}`);
 
 function App() {
+  const [Contacts, setCount] = useState();
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>IronContacts</h1>
+
+      <table>
+        <thead>
+          <tr>
+            <th>Picture</th>
+            <th>Name</th>
+            <th>Popularity</th>
+            <th>Won Oscar</th>
+            <th>Won Emmy</th>
+          </tr>
+        </thead>
+        <tbody>
+          {contacts.map((eachContact, i) => {
+            return (
+              <tr key={i}>
+                <td>
+                  <img
+                    src={eachContact.pictureUrl}
+                    alt="profile"
+                    style={{ height: "100px" }}
+                  />
+                </td>
+                <td>{eachContact.name} </td>
+                <td>{eachContact.popularity.toFixed(2)} </td>
+                {eachContact.wonOscar ? (
+                  <td>🏆</td>
+                ) : (
+                  <td>
+                    <p></p>
+                  </td>
+                )}
+                {eachContact.wonEmmy ? (
+                  <td>🥇</td>
+                ) : (
+                  <td>
+                    <p></p>
+                  </td>
+                )}
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
     </div>
   );
 }
